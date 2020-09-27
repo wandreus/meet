87f2769c684f24e4ca1bd2a50460d7645d4e7620
wandreuspereira <wandreus.pereira@original.io>
2020-09-27 19:28:06 -0300

    feat: adicionando pagina palestrantes

--- src/pages/palestrantes.tsx
+++ src/pages/palestrantes.tsx
@@ -1,3 +1,32 @@
-export default function palestrantes() {
-  return <h1>Palestrantes</h1>
+import Link from 'next/link'
+import { palestrantes } from '../../public/palestrantes.json'
+
+export async function getStaticProps() {
+  return {
+    props: {
+      palestrantes
+    }
+  }
+}
+
+export default function palestrantesPage({ palestrantes }) {
+  type Props = {
+    url: string
+    name: string
+    short_description: string
+  }
+
+  return (
+    <ul>
+      {palestrantes.map(({ name, url }: Props, i: number) => {
+        return (
+          <li key={i}>
+            <Link href={`/palestrantes/${url}`}>
+              <a>{name}</a>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
